c9fc0c38de63bd027256f50b9e24f69a43593564
f-person <agakaryan.arshak@gmail.com>
2021-12-13 17:54:15 +0400

    Add a new sample demonstrating the usage of `KeyboardActionsItem.toolbarAlignment`

---
+++ example/lib/sample4.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:keyboard_actions/keyboard_actions.dart';
+
+/// Sample [Widget] demonstrating the usage of [KeyboardActionsItem.toolbarAlignment].
+class Sample4 extends StatelessWidget {
+  final _focusSample = FocusNode();
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sample 4"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(top: 15.0, left: 15.0, right: 15.0),
+        child: Center(
+          child: KeyboardActions(
+            tapOutsideBehavior: TapOutsideBehavior.translucentDismiss,
+            config: KeyboardActionsConfig(
+              actions: [
+                KeyboardActionsItem(
+                  toolbarAlignment: MainAxisAlignment.spaceAround,
+                  focusNode: _focusSample,
+                  displayArrows: false,
+                  toolbarButtons: [
+                    (_) {
+                      return IconButton(
+                        icon: Icon(Icons.format_bold),
+                        onPressed: () {},
+                      );
+                    },
+                    (_) {
+                      return IconButton(
+                        icon: Icon(Icons.format_italic),
+                        onPressed: () {},
+                      );
+                    },
+                    (_) {
+                      return IconButton(
+                        icon: Icon(Icons.format_underline),
+                        onPressed: () {},
+                      );
+                    },
+                    (_) {
+                      return IconButton(
+                        icon: Icon(Icons.format_strikethrough),
+                        onPressed: () {},
+                      );
+                    },
+                  ],
+                ),
+              ],
+            ),
+            child: ListView(
+              children: [
+                TextField(
+                  controller: _textController,
+                  focusNode: _focusSample,
+                  decoration: InputDecoration(
+                    labelText: "Sample Input",
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
