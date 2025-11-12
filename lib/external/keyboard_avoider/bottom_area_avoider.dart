7d4770ebae6641364d43e993b39f089f9803b51c
luk3d <filipelukebana@gmail.com>
2025-11-12 20:42:21 -0300

    fix: ensure focused state check is explicit in findFocusedObject method

--- lib/external/keyboard_avoider/bottom_area_avoider.dart
+++ lib/external/keyboard_avoider/bottom_area_avoider.dart
@@ -175,7 +175,7 @@ RenderObject? findFocusedObject(RenderObject? root) {
     final config = SemanticsConfiguration();
     //ignore: invalid_use_of_protected_member
     node.describeSemanticsConfiguration(config);
-    if (config.isFocused) {
+    if (config.isFocused == true) {
       return node;
     }
     node.visitChildrenForSemantics((child) {
